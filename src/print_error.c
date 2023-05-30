31bcd245460cf32390794bb74d4604e98c87bcc7
Kenzo Escuret <kenzo.escuret@epitech.eu>
2023-05-30 11:33:09 +0200

    update help section

--- src/print_error.c
+++ src/print_error.c
@@ -31,7 +31,7 @@ int help(char *str)
     if (str[0] != '-') return my_putstr_error("help: '-h'\n");
     if (str[1] != 'h') return my_putstr_error("help: '-h'\n");
     if (str[2] != '\0') return my_putstr_error("help: '-h'\n");
-    my_putstr("RULES:\n    Catch as many pokemon, the more you catch");
+    my_putstr("RULES:\n    Catch as many pokemons, the more you catch");
     my_putstr(", the higher your score.\n\nCONTROLS:\n    Aim with the");
     my_putstr(" mousse.\n    Left click to catch.\n    Esc to quit game.\n");
     return 0;
